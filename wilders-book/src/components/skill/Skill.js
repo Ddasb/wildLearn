cea097050355fde85f201c7e9ab0298dc0ca0fc1
Da Silva Bregieiro Damien <damien.bregieiro@gmail.com>
2020-11-04 16:02:21 +0100

    React base Components

--- wilders-book/src/components/skill/Skill.js
+++ wilders-book/src/components/skill/Skill.js
@@ -1,7 +1,14 @@
+import propTypes from 'prop-types';
+
 function skill({name, votes}) {
     return (
     <li>{name} <span className="votes">{votes}</span></li>
     )
+}
+
+skill.propTypes = {
+    name: propTypes.string.isRequired,
+    votes: propTypes.number.isRequired
 }
 
 export default skill;
